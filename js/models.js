a6ae827098f7a77cd08555d1999f3553f04c88ac
Agamjot Sodhi <146457410+agamjotsodhi@users.noreply.github.com>
2024-09-10 11:34:51 -0400

    Update models.js

--- js/models.js
+++ js/models.js
@@ -3,16 +3,13 @@
 const BASE_URL = "https://hack-or-snooze-v3.herokuapp.com";
 
 /******************************************************************************
- * Main Data handling page has 3 main classes: a) Story b) StoryList c) User
- * STORY: CLASS: a single story in the system
+ * Story: a single story in the system
  */
 
 class Story {
 
   /** Make instance of Story from data object about story:
-   * Single story post template that user inputs info into 
-   *   - {title, author, url, username, storyId, createdAt}
-   * Inital Story post template that will be used to create instances(updated versions with user inputed info) -a.s.
+   *   - {storyId, title, author, url, username, createdAt}
    */
 
   constructor({ storyId, title, author, url, username, createdAt }) {
@@ -24,17 +21,17 @@ class Story {
     this.createdAt = createdAt;
   }
 
-  // Looks at user inputed url, URL object takes only the host name, parses/returns it 
+  /** Parses hostname out of URL and returns it. */
+
   getHostName() {
-    return new URL(this.url).hostname; //parses user inputed url, returns hostname using URL interface - mdn docs
+    return new URL(this.url).host;
   }
 }
+
+
 /******************************************************************************
- 
- * STORYLIST: CLASS:
  * List of Story instances: used by UI to show story lists in DOM.
- * First response - "GET"s story data from API stories endpoint and holds onto it
- * Then we update stories with that response data and includes it in the list of stories */
+ */
 
 class StoryList {
   constructor(stories) {
@@ -52,10 +49,8 @@ class StoryList {
   static async getStories() {
     // Note presence of `static` keyword: this indicates that getStories is
     //  **not** an instance method. Rather, it is a method that is called on the
-    //  class directly. 
-
-    //getStories() is a static method. NOT an intance method, because it is responsible for fetching stories from API endpoint, process response data and create instances of 'Story' class
-    //Operates only on the basic structre NOT on a specific instance of Storylist
+    //  class directly. Why doesn't it make sense for getStories to be an
+    //  instance method?
 
     // query the /stories endpoint (no auth required)
     const response = await axios({
@@ -64,68 +59,60 @@ class StoryList {
     });
 
     // turn plain old story objects from API into instances of Story class
-    //
     const stories = response.data.stories.map(story => new Story(story));
 
     // build an instance of our own class using the new array of stories
     return new StoryList(stories);
   }
 
-  /** Add Story Function
-   * - posts story data to API, makes a Story instance, adds it to story list.
+  /** Adds story data to API, makes a Story instance, adds it to story list.
    * - user - the current instance of User who will post the story
    * - obj of {title, author, url}
-   * returns a new Story instance, response posts story data to api with user instance (token) and story objects: 
-   * updates story class and splices new story to the front of the list  */
+   *
+   * Returns the new Story instance
+   */
 
   async addStory(user, { title, author, url }) {
     const token = user.loginToken;
-
-    // Axios request to post user inputed data back to API /stories endpoint
     const response = await axios({
       method: "POST",
       url: `${BASE_URL}/stories`,
       data: { token, story: { title, author, url } },
     });
 
-    // Update new single story to Story class, returning a new instance 
-    const story = new Story(response.data.story); //adds new story to Story class instance
+    const story = new Story(response.data.story);
+    this.stories.unshift(story);
+    user.ownStories.unshift(story);
 
-    //Add new story to stories and ownStories page using splice array method 
-    this.stories.splice(0, 0, story);
-    user.ownStories.splice(0, 0, story);
-
-    //New Updated story instance
     return story;
   }
 
-  /** Remove Story Function
-  *  - API requires token and delete request to storyId
-  *  - storyId removes single story 
-  */
+  /** Delete story from API and remove from the story lists.
+   *
+   * - user: the current User instance
+   * - storyId: the ID of the story you want to remove
+   */
 
-  async deleteStory(storyId, user) {
+  async removeStory(user, storyId) {
     const token = user.loginToken;
-
-    //Sends axios delete method to API/storyId endpoint
     await axios({
       url: `${BASE_URL}/stories/${storyId}`,
       method: "DELETE",
       data: { token: user.loginToken }
     });
 
-    //Remove deleted story from stories array in StoryList
+    // filter out the story whose ID we are removing
     this.stories = this.stories.filter(story => story.storyId !== storyId);
 
-    //Call functions to remove story from User. own story and favorite pages
-    user.removeFromOwnStory(storyId);
-    user.removeFromFavorites(storyId);
+    // do the same thing for the user's list of stories & their favorites
+    user.ownStories = user.ownStories.filter(s => s.storyId !== storyId);
+    user.favorites = user.favorites.filter(s => s.storyId !== storyId);
   }
 }
 
 
 /******************************************************************************
- * USER: CLASS: a user in the system (only used to represent the current user)
+ * User: a user in the system (only used to represent the current user)
  */
 
 class User {
@@ -135,13 +122,13 @@ class User {
    */
 
   constructor({
-    username,
-    name,
-    createdAt,
-    favorites = [],
-    ownStories = []
-  },
-    token) {
+                username,
+                name,
+                createdAt,
+                favorites = [],
+                ownStories = []
+              },
+              token) {
     this.username = username;
     this.name = name;
     this.createdAt = createdAt;
@@ -168,7 +155,7 @@ class User {
       data: { user: { username, password, name } },
     });
 
-    let { user } = response.data
+    let { user } = response.data;
 
     return new User(
       {
@@ -239,71 +226,42 @@ class User {
     }
   }
 
-  /** Favourite and Unfavorite Story Functions
-   * - add feature to mark/unmark favourite
-   * - token for user instance
-   * - API favorite parameters: username, storyId. Post
-   * - API unfavorite parameters: username, storyId. Delete
+  /** Add a story to the list of user favorites and update the API
+   * - story: a Story instance to add to favorites
    */
 
-  async favoritedStory(story) {
-    const token = this.loginToken;
-
-    //Axios method to post a favorite action to the API
-    await axios({
-      url: `${BASE_URL}/users/${this.username}/favorites/${story.storyId}`,
-      method: "POST",
-      data: { token },
-    });
-
-    //adds story to favorites array
+  async addFavorite(story) {
     this.favorites.push(story);
+    await this._addOrRemoveFavorite("add", story)
   }
 
-  //Unfavorites a story that was previously favorited
-  async deleteFavoriteStory(story) {
+  /** Remove a story to the list of user favorites and update the API
+   * - story: the Story instance to remove from favorites
+   */
+
+  async removeFavorite(story) {
+    this.favorites = this.favorites.filter(s => s.storyId !== story.storyId);
+    await this._addOrRemoveFavorite("remove", story);
+  }
+
+  /** Update API with favorite/not-favorite.
+   *   - newState: "add" or "remove"
+   *   - story: Story instance to make favorite / not favorite
+   * */
+
+  async _addOrRemoveFavorite(newState, story) {
+    const method = newState === "add" ? "POST" : "DELETE";
     const token = this.loginToken;
     await axios({
       url: `${BASE_URL}/users/${this.username}/favorites/${story.storyId}`,
-      method: "DELETE",
+      method: method,
       data: { token },
     });
-
-    //removes story from favorite array 
-    this.favorites = this.favorites.filter(s => s.storyId !== story.storyId);
   }
 
-  // Determines favorite status: returns true if story id is present user.favorites, false if no
+  /** Return true/false if given Story instance is a favorite of this user. */
+
   isFavorite(story) {
     return this.favorites.some(s => (s.storyId === story.storyId));
   }
-
-  //For Storylist Class deleteStory() function:
-  /** Remove Story From Favorite Page and OwnStories when user deletes a story
-      - method that is called from the StoryList class to filter/delete a given story from pages related to user*/
-      
-  removeFromFavorites(storyId) {
-    this.favorites = this.favorites.filter(story => story.storyId !== storyId);
-  }
-
-  /** Remove Story Method From OwnStories Page */
-  removeFromOwnStory(storyId) {
-    this.ownStories = this.ownStories.filter(story => story.storyId !== storyId);
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
